091761a51042c4a1241dd60610fae26f9d8720f7
jessinra <jessinra@gmail.com>
2020-02-26 12:39:20 +0700

    push

--- py_train.py
+++ py_train.py
@@ -16,13 +16,16 @@ PARAMS = {
     "rnn_type": "LSTM",
     "tokenizer": "BERT",
     "word_embedding": "BERT",
-    "vocab_size": 3000,
-    "combine_strategy": "inject_par",
+    "vocab_size": 5000,
+    "combine_strategy": "merge",
     "combine_layer": "concat",
     "image_context_size": 256,
     "word_embedding_dim": 256,
-    "batch_size": 32,
+    "batch_size": 64,
+    "data_size": "all",
+    "use_mapping": True,
 }
+
 
 # In[ ]:
 
@@ -49,9 +52,9 @@ import keras as K
 # In[ ]:
 
 
-config = tf.ConfigProto()
+config = tf.compat.v1.ConfigProto()
 config.gpu_options.allow_growth=True
-session = tf.Session(config=config)
+session = tf.compat.v1.Session(config=config)
 
 
 # In[ ]:
@@ -82,9 +85,6 @@ with open(annotation_file, 'r') as f:
 # In[ ]:
 
 
-NUM_SAMPLES = 1200
-
-
 # Store captions and image names
 all_captions = []
 all_img_paths = []
@@ -99,16 +99,23 @@ for annot in annotations['annotations']:
 
 # Shuffle captions and image_names together
 all_captions, all_img_paths = shuffle(all_captions, all_img_paths, random_state=1)
-train_captions = all_captions
-train_img_paths = all_img_paths
 
-NUM_SAMPLES = len(train_img_paths)
+stopper = -1 if PARAMS["data_size"] == "all" else PARAMS["data_size"]
+train_captions = all_captions[:stopper]
+train_img_paths = all_img_paths[:stopper]
+
 
 # In[ ]:
 
 
 print("len train_captions :", len(train_img_paths))
 print("len all_captions :", len(all_img_paths))
+
+
+# In[ ]:
+
+
+DATA_SIZE = len(train_captions) if PARAMS["data_size"] == "all" else PARAMS["data_size"]
 
 
 # ## Image feature extractor
@@ -184,7 +191,7 @@ image_dataset = image_dataset.batch(BATCH_SIZE)
 # In[ ]:
 
 
-estimated_batch_count = NUM_SAMPLES / BATCH_SIZE + 1
+estimated_batch_count = DATA_SIZE / BATCH_SIZE + 1
 print("estimated_batch_count", estimated_batch_count)
 
 
@@ -231,17 +238,118 @@ VOCAB_SIZE = PARAMS["vocab_size"]  # Choose the top-n words from the vocabulary
 
 
 class BertTokenizerWrapper(BertTokenizer):
+    
+    def use_custom_mapping(self, use_mapping=True, vocab_size=3000):
+        self.use_mapping = use_mapping
+        self.vocab_size = vocab_size
+        self.mapping_initialized = False
 
+        
     def texts_to_sequences(self, texts):
         """
-        convert batch texts into indexed version
+        convert batch texts into custom indexed version
         eg: ['an apple', 'two person']
         output: [[1037,17260], [2083, 2711]] 
         """
         
-        tokenized_texts = [self.tokenize(x) for x in texts]
-        token_ids = [self.convert_tokens_to_ids(x) for x in tokenized_texts]
+        bert_ids = [self.convert_tokens_to_ids(self.tokenize(x)) for x in tqdm(texts)]
         
+        if not self.use_mapping:
+            return bert_ids
+        
+        if not self.mapping_initialized:
+            self._initialize_custom_mapping(bert_ids)
+            return [self._convert_bert_id_to_custom_id(x) for x in bert_ids]
+    
+        return bert_ids
+    
+        
+    def convert_tokens_to_ids(self, tokens):
+        
+        bert_ids = super().convert_tokens_to_ids(tokens)
+        
+        if self.use_mapping and self.mapping_initialized:
+            return self._convert_bert_id_to_custom_id(bert_ids)
+        else:
+            return bert_ids
+        
+        
+    def convert_ids_to_tokens(self, token_ids):
+        
+        if self.use_mapping and self.mapping_initialized:
+            bert_ids = self._convert_custom_id_to_bert_id(token_ids)
+        else:
+            bert_ids = token_ids
+            
+        bert_tokens = super().convert_ids_to_tokens(bert_ids)
+        return bert_tokens
+    
+    
+    def _initialize_custom_mapping(self, corpus_bert_ids):
+        
+        print("    > constructing custom mapping < \n")
+        self._build_occurence_table(corpus_bert_ids)
+        self._build_custom_mapping_table()
+        self.mapping_initialized = True
+        
+        
+    def _build_occurence_table(self, tokenized_captions):
+        """
+        build dict of token frequency
+        """
+        
+        self.occurence_table = {}
+        for caption in tqdm(tokenized_captions):
+            for token in caption:
+                if token not in self.occurence_table:
+                    self.occurence_table[token] = 0
+                self.occurence_table[token] += 1
+    
+    
+    def _set_vocab_size(self, vocab_size):
+        self.vocab_size = vocab_size
+    
+    
+    def _build_custom_mapping_table(self):
+            
+        self.bert_id_to_custom_id = {0:0}
+        self.custom_id_to_bert_id = {0:0}
+        
+        sorted_occurence = {k: v for k, v in sorted(
+            self.occurence_table.items(), reverse=True, key=lambda item: item[1]
+        )}
+        
+        used_tokens = sorted(list(sorted_occurence)[:self.vocab_size])
+        
+        for i in range(0, min(len(used_tokens), self.vocab_size)):
+            bert_token = used_tokens[i]
+            self.bert_id_to_custom_id[bert_token] = i + 1    # 0 for padding
+            self.custom_id_to_bert_id[i + 1] = bert_token    # 0 for padding
+            
+        print("Vocab contains {0} / {1} unique tokens ({2:.2f} %)".format(
+            len(used_tokens),\
+            len(sorted_occurence),\
+            (len(used_tokens) / len(sorted_occurence) * 100)
+        ))
+        
+        sorted_occurence_count = list(sorted_occurence.values())
+        used_tokens_count = sum(sorted_occurence_count[:self.vocab_size])
+        total_tokens_count = sum(sorted_occurence_count)
+        
+        print("Using {0} / {1} tokens available ({2:.2f} %)".format(
+            used_tokens_count,\
+            total_tokens_count,\
+            (used_tokens_count / total_tokens_count * 100)
+        ))
+        
+    def _convert_bert_id_to_custom_id(self, token_ids):
+        
+        token_ids = [self.bert_id_to_custom_id[x] if x in self.bert_id_to_custom_id else 0 for x in token_ids]
+        return token_ids
+                    
+    def _convert_custom_id_to_bert_id(self, token_ids):  
+        
+        token_ids = [self.custom_id_to_bert_id[x] for x in token_ids]
         return token_ids
 
 
@@ -257,17 +365,21 @@ class TokenizerWrapper(Tokenizer):
 # In[ ]:
 
 
-def get_tokenizer(tokenizer_type="BERT"):
+def get_tokenizer():
     
-    if tokenizer_type == "BERT" :
+    if PARAMS["tokenizer"] == "BERT" :
 
         # Load pre-trained BERT tokenizer (vocabulary)
         tokenizer = BertTokenizerWrapper.from_pretrained('bert-base-uncased')
+        tokenizer.use_custom_mapping(
+            use_mapping=PARAMS["use_mapping"],
+            vocab_size=PARAMS["vocab_size"]
+        )
 
     else : 
 
         # use default keras tokenizer
-        tokenizer = TokenizerWrapper(num_words=VOCAB_SIZE, oov_token="[UNK]")
+        tokenizer = TokenizerWrapper(num_words=PARAMS["vocab_size"], oov_token="[UNK]")
         tokenizer.fit_on_texts(train_captions)    
         tokenizer.word_index['[PAD]'] = 0
         tokenizer.index_word[0] = '[PAD]'
@@ -278,7 +390,7 @@ def get_tokenizer(tokenizer_type="BERT"):
 # In[ ]:
 
 
-tokenizer = get_tokenizer(tokenizer_type=TOKENIZER)
+tokenizer = get_tokenizer()
 train_captions = tokenizer.texts_to_sequences(train_captions)
 
 
@@ -317,8 +429,7 @@ def load_image_npy(img_name, cap):
 
 # Create dataset object
 
-from tensorflow.data import Dataset
-dataset = Dataset.from_tensor_slices((train_img_paths, train_captions))
+dataset = tf.data.Dataset.from_tensor_slices((train_img_paths, train_captions))
 
 
 # In[ ]:
@@ -352,7 +463,7 @@ TRAIN_SPLIT = 0.7
 EVAL_SPLIT = 0.15
 TEST_SPLIT = 0.15  # approx
 
-n_batch = int(NUM_SAMPLES / BATCH_SIZE) + 1
+n_batch = int(DATA_SIZE / BATCH_SIZE) + 1
 n_train = int(n_batch * 0.7)
 n_eval = int(n_batch * 0.15)
 n_test = n_batch - (n_train + n_eval)
@@ -509,7 +620,7 @@ class RNN_Decoder(tf.keras.Model):
     def __init__(self, rnn_type="LSTM", rnn_units=256, 
                  embedding_type="BERT", embedding_dim=256, 
                  combine_strategy="merge", combine_layer="concat",
-                 vocab_size=5000):
+                 vocab_size=3000, batch_size=32):
         
         super(RNN_Decoder, self).__init__()
         self.rnn_units = rnn_units
@@ -517,6 +628,7 @@ class RNN_Decoder(tf.keras.Model):
         self.embedding_type = embedding_type
         self.embedding_dim = embedding_dim
         self.vocab_size = vocab_size
+        self.batch_size = batch_size
         
         # when to use context_vector ["inject_init", "inject_pre", "inject_par", "merge"]
         self.combine_strategy = combine_strategy
@@ -554,7 +666,7 @@ class RNN_Decoder(tf.keras.Model):
             self.bert_embedding.to('cuda')
             
             self.embedding_dim = self.bert_embedding.config.hidden_size
-            self.vocab_size = self.bert_embedding.config.vocab_size
+            # self.vocab_size = self.bert_embedding.config.vocab_size
             
         else:
             self.default_embedding = Embedding(self.vocab_size, self.embedding_dim)
@@ -615,8 +727,9 @@ class RNN_Decoder(tf.keras.Model):
     
     def apply_strategy(self, x, context_vector, curr_iter=0):
         """
-        x              : rnn input (word embedding)
         context_vector : image's vector
+        x              : rnn input (word embedding)
+        strategy       : 
         curr_iter      : current iteration number
         """
         
@@ -706,11 +819,21 @@ class RNN_Decoder(tf.keras.Model):
         
         return word_predictions, rnn_state
 
-    def reset_state(self, batch_size):
-        return tf.zeros((batch_size, self.rnn_units))
+    def reset_state(self, batch_size=None):
+        
+        if batch_size is not None:
+            return tf.zeros((batch_size, self.rnn_units))
+        
+        return tf.zeros((self.batch_size, self.rnn_units))        
 
 
 # ## Define model
+
+# In[ ]:
+
+
+PARAMS
+
 
 # In[ ]:
 
@@ -730,7 +853,8 @@ decoder = RNN_Decoder(
     embedding_dim=PARAMS["word_embedding_dim"],  
     combine_strategy=PARAMS["combine_strategy"], 
     combine_layer=PARAMS["combine_layer"],
-    vocab_size=PARAMS["vocab_size"]
+    vocab_size=PARAMS["vocab_size"],
+    batch_size=PARAMS["batch_size"]
 )
 
 
@@ -752,11 +876,10 @@ decoder = RNN_Decoder(
 def feed_forward(img_tensor, target):
     
     loss = 0
-    batch_size = target.shape[0]
     
     # initializing the hidden state for each batch
     # because the captions are not related from image to image
-    hidden = decoder.reset_state(batch_size=batch_size)
+    hidden = decoder.reset_state()
 
     ## decoder_input == last word generated
     decoder_input = tf.expand_dims(target[:, 0], 1)
@@ -923,9 +1046,6 @@ def get_supporting_features(images_paths, strategy="mean"):
 # In[ ]:
 
 
-from tensorflow.sparse import SparseTensor
-
-
 def get_one_hot_indices(support_text):
     
     indices = []
@@ -949,7 +1069,7 @@ def get_supporting_text_vector(support_text, vocab_size):
     # indices => ( sum(batch_size * ?word_count), 2)
     indices = get_one_hot_indices(support_text)
     values = tf.ones(len(indices))
-    sparse_one_hot = SparseTensor(indices, values, dense_shape=[batch_size, vocab_size])
+    sparse_one_hot = tf.sparse.SparseTensor(indices, values, dense_shape=[batch_size, vocab_size])
     
     # sparse_one_hot => (batch_size, vocab_size)
     return sparse_one_hot
@@ -980,6 +1100,7 @@ def custom_evaluate(images_paths,
     """
     
     batch_size = len(images_paths)
+    
     attention_plot = np.zeros((batch_size, MAX_CAPTION_LENGTH, ATTENTION_SHAPE))
 
     # Extract features from main images
@@ -1103,16 +1224,7 @@ def pplm_loss_function(real, pred, pplm_weight=0.03):
     """
 
 
-# !git add "Keras.ipynb"
-# !git commit -m "added sampling top-k method"
-
 # ## Setup training
-
-# In[ ]:
-
-
-# ! rm -rf checkpoints
-
 
 # In[ ]:
 
@@ -1136,30 +1248,33 @@ print(checkpoint_path)
 print("----------------------")
 
 
+# In[ ]:
+
+
+start_epoch = 0
+loss_plot = []
+
+
+# In[ ]:
+
+
 import os
 
 os.mkdir(checkpoint_path)
 with open(checkpoint_path + "/config.txt", "w") as f:
     f.write(str(PARAMS))
+    
+log_file = open(checkpoint_path + "/log.txt", "a")
 
 
 # In[ ]:
 
 
-start_epoch = 0
 if ckpt_manager.latest_checkpoint:
     start_epoch = int(ckpt_manager.latest_checkpoint.split('-')[-1])
     
     # restoring the latest checkpoint in checkpoint_path
     ckpt.restore(ckpt_manager.latest_checkpoint)
-
-
-# In[ ]:
-
-
-# adding this in a separate cell because if you run the training cell
-# many times, the loss_plot array will be reset
-loss_plot = []
 
 
 # ## Actual training
@@ -1181,13 +1296,15 @@ for epoch in range(start_epoch, EPOCHS):
         total_loss += t_loss
 
         if batch % 50 == 0:
-            print ('{} Epoch {} Batch {} Loss {:.4f}'.format(checkpoint_path, epoch + 1, batch, batch_loss.numpy() / int(target.shape[1])))   
-        
+            
+            log_message = '{} Epoch {} Batch {} Loss {:.4f}'.format(
+                checkpoint_path, epoch + 1, batch, batch_loss.numpy() / int(target.shape[1]))
+            
+            print (log_message)   
+            log_file.write(str(log_message + "\n"))
+            
         batch += 1
         
-        # storing the epoch end loss value to plot later
-        loss_plot.append(batch_loss)
-
     ckpt_manager.save()
 
     print ('Epoch {} Loss {:.6f}'.format(epoch + 1, total_loss))
